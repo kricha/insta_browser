f56c996f2b60cfe4f03bd54c48af97849362a37e
Aleksej <krich.al.vl@gmail.com>
2017-06-25 23:04:49 +0300

    rewrite main module

--- insta_browser/browser.py
+++ insta_browser/browser.py
@@ -1,96 +1,62 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-import pickle
 import time
 import random
+from .logger import Logger
+from .configure import *
+from .auth import *
 
 
 class Browser:
 
-    def __init__(self, debug=False, chrome=False, cookie_path='/tmp'):
+    def __init__(self, debug=False, chrome=False, cookie_path=None, screen_shot_path=None, logger_file=None, exclude=None):
         if chrome:
-            self.chrome = chrome
             self.browser = webdriver.Chrome()
         else:
-            headers = {
-                'Accept': '*/*',
-                'Accept-Language': 'en-US,en;q=0.8',
-                'Cache-Control': 'max-age=0',
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-            }
-            for key, value in headers.items():
-                webdriver.DesiredCapabilities.PHANTOMJS['phantomjs.page.customHeaders.{}'.format(key)] = value
-            webdriver.DesiredCapabilities.PHANTOMJS['phantomjs.page.settings.userAgent'] = headers.get('User-Agent')
-            self.browser = webdriver.PhantomJS()
-            self.browser.command_executor._commands['executePhantomScript'] = ('POST', '/session/$sessionId/phantom/execute')
-            self.resource_requested_logic()
-        self.debug = debug
+            self.browser = set_headers(webdriver)
+            self.browser.command_executor._commands['executePhantomScript'] = ('POST',
+                                                                               '/session/$sessionId/phantom/execute')
+            resource_requested_logic(self.browser)
         self.cookie_path = cookie_path
+        self.exclude = exclude or []
+        self.chrome = chrome
+        self.logger = Logger(logger_file, screen_shot_path, debug)
+        self.__setup_initial_props()
+
+    def __setup_initial_props(self):
+        """
+        Setup initial properties
+        :return: Browser
+        """
+        self.login = ''
         self.liked = 0
         self.skipped = 0
         self.feed_scrolled_down = 0
-        self.login = ''
+        self.skipped_excluded = 0
+        self.feed_posts_per_page = 12
+        self.posts_count_to_like = None
 
-    def clear_driver_cache(self):
-        self.browser.execute('executePhantomScript', {'script': '''
-            var page = this;
-            page.clearMemoryCache();
-        ''', 'args': []})
-
-    def resource_requested_logic(self):
-        self.browser.execute('executePhantomScript', {'script': '''
-            var page = this;
-            page.onResourceRequested = function(request, networkRequest) {
-                if (/\.(jpg|jpeg|png|mp3|css|mp4)/i.test(request.url))
-                {
-                    //console.log('Final with css! Suppressing image: ' + request.url);
-                    networkRequest.abort();
-                    return;
-                }
-            }
-        ''', 'args': []})
+        return self
 
     def auth(self, login, password):
         br = self.browser
         self.get("https://www.instagram.com/accounts/login/")
-        self.browser.save_screenshot('var/screenshots/login.png')
-        time.sleep(2)
-        try:
-            self.log('try to auth with cookies')
-            cookies = pickle.load(open('{}/{}.pkl'.format(self.cookie_path, login), "rb"))
-            for cookie in cookies:
-                br.add_cookie(cookie)
-
-        except:
-            self.log('regular login')
-            login_field = br.find_element_by_name("username")
-            login_field.clear()
-            self.log(u'Login fill')
-            login_field.send_keys(login)
-            password_field = br.find_element_by_name("password")
-            password_field.clear()
-            self.log(u'Password fill')
-            password_field.send_keys(password)
-            submit = br.find_element_by_css_selector("form button")
-            self.log(u'Form submit')
-            submit.submit()
-            time.sleep(3)
-            pickle.dump([br.get_cookie('sessionid')], open('{}/{}.pkl'.format(self.cookie_path, login), "wb"))
-            self.log('auth complete')
-        br.refresh()
+        time.sleep(1)
+        if not auth_with_cookies(br, self.logger, login, self.cookie_path):
+            auth_with_credentials(br, self.logger, login, password, self.cookie_path)
         self.login = login
 
     def get(self, url):
         self.browser.get(url)
-        self.log(u'Open ' + self.browser.current_url)
+        self.logger.log(u'Open ' + self.browser.current_url)
         return self
 
     def close_all(self):
-        self.browser.save_screenshot('var/screenshots/close_all.png')
+        self.logger.save_screen_shot(self.browser, 'exit.png')
         self.browser.close()
         self.browser.quit()
-        self.log(u'Browser process was ended')
-        self.log(u'')
+        self.logger.log(u'Browser process was ended')
+        self.logger.log(u'')
 
     def is_last_post_in_feed_not_liked(self):
         try:
@@ -99,42 +65,67 @@ class Browser:
         except:
             return False
 
-    def scroll_feed_to_last_not_liked(self):
+    def scroll_feed_to_last_not_liked_posts(self):
         while self.is_last_post_in_feed_not_liked():
             self.browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             time.sleep(1)
             self.feed_scrolled_down += 1
 
+    def scroll_feed_by_posts_count(self, posts_count):
+        self.posts_count_to_like = posts_count
+        times_to_scroll = posts_count/12
+        while times_to_scroll > 0:
+            self.browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(1)
+            self.feed_scrolled_down += 1
+            times_to_scroll -= 1
+
     def like_found_posts(self):
         br = self.browser
         articles = br.find_elements_by_tag_name('article')
-        for post in articles:
-            sec = random.randint(1, 15)/float(10)
-            time.sleep(sec)
-            mouse = webdriver.ActionChains(br).move_to_element(post)
+        posts = self.preprocess_posts_from_feed(articles)
+        del articles
+        for post in posts:
+            webdriver.ActionChains(br).move_to_element(post['heart']).perform()
+            post['heart'].click()
+            self.liked += 1
+            self.logger.log('\t♥️  @{} post {}'.format(post['author'], post['link']))
+            time.sleep(random.randint(1, 10)/float(10))
+
+    def preprocess_posts_from_feed(self, posts):
+        posts_to_perform = []
+        posts_count = self.posts_count_to_like or len(posts)
+        i = 1
+        for post in posts:
+            if i > posts_count:
+                break
             heart = post.find_element_by_css_selector('div:nth-child(3) section a:first-child')
             author = post.find_element_by_css_selector('div:first-child .notranslate').text
             heart_classes = heart.find_element_by_css_selector('span').get_attribute("class")
             if 'coreSpriteHeartOpen' in heart_classes and author != self.login:
-                post_link = self.get_feed_post_link(post)
-                self.log('\t♥️  @{} post {}'.format(author, post_link))
-                mouse.move_to_element(heart).perform()
-                heart.click()
-                self.liked += 1
+                if author in self.exclude:
+                    self.skipped_excluded += 1
+                else:
+                    post_link = self.get_feed_post_link(post)
+                    posts_to_perform.append({'heart': heart, 'author': author, 'link': post_link})
             else:
                 self.skipped += 1
+            i += 1
+
+        return posts_to_perform
 
     def get_summary(self):
-        return 'Feed scrolled down {} times, liked {} posts, skipped {} posts'.\
-            format(self.feed_scrolled_down, self.liked, self.skipped)
+        return 'Feed scrolled down {} times, liked {} posts, skipped {} posts, skipped excluded {} posts'.\
+            format(self.feed_scrolled_down, self.liked, self.skipped, self.skipped_excluded)
 
     def get_feed_post_link(self, post):
+        """
+        Get link to post from post web-element from feed
+        :param post:
+        :return:
+        """
         try:
             post_link = post.find_element_by_css_selector('div:nth-child(3) div:nth-child(4) a')
         except:
             post_link = post.find_element_by_css_selector('div:nth-child(3) div:nth-child(3) a')
         return post_link.get_attribute('href')
-
-    def log(self, text):
-        if self.debug:
-            print(text)
