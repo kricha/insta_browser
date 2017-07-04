c33d60b0899670c18fd9811c68def4358255ac39
Aleksej <krich.al.vl@gmail.com>
2017-07-04 21:00:18 +0300

    refactor non feed processor

--- insta_browser/insta_not_feed_util.py
+++ insta_browser/insta_not_feed_util.py
@@ -2,44 +2,47 @@ import tqdm
 import time
 from selenium.webdriver.common.action_chains import ActionChains
 import selenium.common.exceptions as excp
+from .base.processor import BaseProcessor
+import re
 
 TOP_POSTS_XPATH = '//*[@id="react-root"]/section/main/article/div[1]/div'
 LATEST_POSTS_XPATH = '//*[@id="react-root"]/section/main/article/div[2]'
+POST_CSS_SELECTOR = '#fb-root + div article'
 
 
-class NotFeedProcessor:
-    liked = 0
-    skipped = 0
-    progress = None
-    heart = None
-    already_liked = 0
-    count = 0
-
+class NotFeedProcessor(BaseProcessor):
     def __init__(self, browser, logger):
         self.browser = browser
         self.logger = logger
 
     def like_user_profile(self, count):
+        if count:
+            posts_count = count
+        else:
+            posts_from_page = self.browser.find_element_by_css_selector("article header ul li span").text
+            tmp_count = int(re.match('\d+', posts_from_page).group(0))
+            posts_count = self.like_limit if tmp_count > self.like_limit else tmp_count
         self.browser.find_element_by_css_selector("article > div a").click()
-        self.go_through_posts(count)
-        return self.__return_liking_summary()
+        self.go_through_posts(posts_count)
 
     def like_top(self):
-        self.logger.log('Start processing top posts')
+        self.logger.log('Start processing top posts.')
         self.__get_posts_block(TOP_POSTS_XPATH)
         self.go_through_posts(9)
-        return self.__return_liking_summary()
 
     def like_latest(self, count):
-        self.logger.log('Start processing lastest posts')
+        if count:
+            posts_count = self.like_limit - 9 if count > self.like_limit else count - 9
+        else:
+            posts_count = self.like_limit - 9
+        self.logger.log('Start processing latest posts.')
         self.__get_posts_block(LATEST_POSTS_XPATH)
-        self.go_through_posts(count)
-        return self.__return_liking_summary()
+        self.go_through_posts(posts_count)
 
     def go_through_posts(self, count):
         self.count = count
         time.sleep(.5)
-        self.already_liked = 0
+        self.post_already_liked = 0
         progress = tqdm.tqdm(range(self.count))
         for i in progress:
             time.sleep(1)
@@ -50,32 +53,35 @@ class NotFeedProcessor:
             progress.update()
 
     def __like_post(self):
+        """
+        Like posts or skip
+
+        :return:
+        """
         if self.__is_not_liked_acc_post():
             self.heart.click()
-            self.logger.log_to_file("--> like post")
-            self.liked += 1
-            self.already_liked = 0
+            self.logger.log_to_file('--> like post {}'.format(self.browser.current_url))
+            self.post_liked += 1
+            self.post_already_liked = 0
             time.sleep(0.5)
         elif not self.heart and self.count > 9:
-            self.already_liked += 1
-            self.skipped +=1
+            self.post_already_liked += 1
+            self.post_skipped +=1
         else:
-            self.skipped += 1
+            self.post_skipped += 1
 
     def __go_to_next_post(self):
         """
         Go to next post on non-feed page
+
         :return:
         """
         link = self.__has_next()
-        if not link or self.already_liked > 5:
+        if not link or self.post_already_liked > 4:
             return False
         else:
             link.click()
             return True
-
-    def __return_liking_summary(self):
-        return {'liked': self.liked, 'skipped': self.skipped}
 
     def __get_posts_block(self, block_xpath):
         top_block = self.browser.find_element_by_xpath(block_xpath)
@@ -85,6 +91,7 @@ class NotFeedProcessor:
     def __is_not_liked_acc_post(self):
         """
         Check if not feed post is liked
+
         :return: like WebElement if exist or False if not
         """
         self.heart = None
@@ -98,6 +105,7 @@ class NotFeedProcessor:
     def __has_next(self):
         """
         Check if page has nex link
+
         :return: next link WebElement if exist and False if not
         """
         try:
